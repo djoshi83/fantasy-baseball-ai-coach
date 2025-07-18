285c8fd6361f6c67ed0a88835e25b49ee20f86c6
djoshi83 <devanjoshi98@gmail.com>
2025-07-17 21:49:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ for _, row in pitchers.iterrows():
 
 # --- Display today's probable pitchers ---
 st.subheader("🎯 Next Scheduled Probable Pitchers")
-pitchers = get_probable_pitchers(formatted_date)
+pitchers, schedule_data = get_probable_pitchers(formatted_date)
 
 for team, pitcher_name in pitchers.items():
     st.write(f"{team}: {pitcher_name}")
